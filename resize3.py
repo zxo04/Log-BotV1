c16060ca653ecc88557f9b2b9b6255436d1214fd
zxo04 <37088414+zxo04@users.noreply.github.com>
2023-03-24 01:24:42 +0000

    Update resize3.py

--- resize3.py
+++ resize3.py
@@ -1,9 +1,9 @@
 from PIL import Image
 
-img = Image.open('./img/right.jpg')
+img = Image.open('right.jpg')
 
 box = (375, 175, 800, 1050)
 img2 = img.crop(box)
 
 img2.show()
-img2.save('./img/output.jpg')
+img2.save('output.jpg')
